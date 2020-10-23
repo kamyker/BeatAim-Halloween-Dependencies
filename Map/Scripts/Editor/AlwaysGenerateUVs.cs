437aa721d529b1f7feca611bd08550f86ffc50d1
kamyker <kamyker@o2.pl>
2020-10-23 22:49:56 +0200

    init

---
+++ Map/Scripts/Editor/AlwaysGenerateUVs.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AlwaysGenerateUVs : AssetPostprocessor
+{
+	private void OnPreprocessModel()
+	{
+		Debug.Log( $"name: {assetImporter.name}" );
+		ModelImporter modelImporter = assetImporter as ModelImporter;
+		if ( modelImporter != null )
+		{
+			modelImporter.generateSecondaryUV = true;
+		}
+	}
+}
